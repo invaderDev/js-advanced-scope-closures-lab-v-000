228d00dc94692a286fcae772a1048053f714a698
Kris Thompson <kt.devcat@gmail.com>
2018-01-19 20:01:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function produceDrivingRange(blockRange){
   return function(start, end) {
     let distance = Math.abs(parseInt(start) - parseInt(end));
     if (distance < blockRange) {
-      return `withing range by ` + (blockRange - distance);
+      return `within range by ` + (blockRange - distance);
     }
     else {
       return (distance - blockRange) + ` blocks out of range`;
