33184cb9406350168e8a8f02f98eab4f894d573d
Kris Thompson <kt.devcat@gmail.com>
2018-01-19 20:05:16 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function produceDrivingRange(blockRange){
+  return function(start, end) {
+    let distance = Math.abs(parseInt(start) - parseInt(end));
+    if (distance < blockRange) {
+      return `within range by ` + (blockRange - distance);
+    }
+    else {
+      return (distance - blockRange) + ` blocks out of range`;
+    }
+  }
+}
+
+function produceTipCalculator(tipPercentage){
+  return function(fare){
+    return fare * tipPercentage;
+  }
+}
+
+function createDriver(){
+  let driverId = 0;
+  return class {
+    constructor(name){
+      this.name = name;
+      this.id = ++driverId;
+    }
+  }
+}
