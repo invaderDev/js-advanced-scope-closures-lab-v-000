d7d1072e4ae0b79ce3184fb30d0d26cd76a6b450
Kris Thompson <kt.devcat@gmail.com>
2018-01-19 20:04:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,12 @@ function produceTipCalculator(tipPercentage){
     return fare * tipPercentage;
   }
 }
+
+function createDriver(){
+  let driverId = 0;
+  return class {
+    constructor(){
+      this.id = ++driverId;
+    }
+  }
+}
